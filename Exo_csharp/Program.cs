81adfbc2cfda6de544a9ef13c75dcf8c6db10a5d
Hantz1 <jeffhantzbelizaire604@gmail.com>
2022-04-21 22:25:47 -0400

    Les essaies sur csharp

--- Exo_csharp/Program.cs
+++ Exo_csharp/Program.cs
@@ -4,10 +4,65 @@ namespace Exo_csharp
 {
     class Program
     {
+        //List<string> ListPrenom=new List<string>();
+        
         static void Main(string[] args)
         {
-            var nom="Jeff";
-            Console.WriteLine("Le nom du devloppeur est: "+nom);
+            //Les Tableaux
+            // String[] nom=new String[]{"Jeff","Hantz"};
+            // int[] nombre=new int[5];
+            // nombre[0]=56;
+            // nombre[1]=30;
+            // nombre[2]=23;
+            // nombre[3]=20;
+            // nombre[4]=33;
+            // for(int i=0;i<nom.Length;i++){
+            //     Console.WriteLine(nom[i]);
+            // }
+            // for(int i=0;i<nombre.Length;i++){
+            //     Console.WriteLine(nombre[i]);
+            // }
+
+            //Les Listes. Nb: La nombre d'element dans une liste "Count"
+            // List<string> Prenom;
+            // Prenom=Ajouter();
+            // foreach(string pre in Prenom){
+            //     Console.WriteLine(pre);
+            // }
+            
+
+            //Enum
+            // Login inscrit;
+            // inscrit=Login.pseudo;
+            // Console.WriteLine(inscrit);
+
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(Environment.UserName);
+
+            //Les converions
+            // int nbre=21;
+            // double nb=double.parse(nbre);
+            Console.WriteLine("Saisie votre nom");
+            string nom=Console.ReadLine();
+            Console.WriteLine("Ton nom est "+nom);
+
+
         }
+
+        //Les enumerations
+        // enum Login{
+        //         pseudo,
+        //         nom,
+        //         prenom,
+        //         password,
+        //     }
+
+
+        // static List<string> Ajouter(){
+        //     ListPrenom.add("Jeff");
+        //     ListPrenom.add("Hantz");
+        //     ListPrenom.add("Belizaire");
+        //     return ListPrenom;
+        // }
     }
 }
